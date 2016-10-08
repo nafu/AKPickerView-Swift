d224a3d84bf0c03b522878b9d11d309a2e07d5e0
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-10-08 18:12:40 +0900

    Update sample viewcontroller

--- AKPickerViewSample/ViewController.swift
+++ AKPickerViewSample/ViewController.swift
@@ -21,8 +21,6 @@ class ViewController: UIViewController, AKPickerViewDataSource, AKPickerViewDele
 
 		self.pickerView.font = UIFont(name: "HelveticaNeue-Light", size: 20)!
 		self.pickerView.highlightedFont = UIFont(name: "HelveticaNeue", size: 20)!
-		self.pickerView.pickerViewStyle = .Wheel
-		self.pickerView.maskDisabled = false
 		self.pickerView.reloadData()
 	}
 
